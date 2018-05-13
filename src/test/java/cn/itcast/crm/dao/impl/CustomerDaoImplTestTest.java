08f3b8fa66dc92e6d8bdac5bf3e8c064d0669e3f
lx <lx@DESKTOP-VDP0HBF>
2018-05-13 17:20:26 +0800

    Test

---
+++ src/test/java/cn/itcast/crm/dao/impl/CustomerDaoImplTestTest.java
@@ -0,0 +1,14 @@
+package cn.itcast.crm.dao.impl;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CustomerDaoImplTestTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
